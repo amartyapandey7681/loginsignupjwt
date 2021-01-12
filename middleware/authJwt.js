b0ac33c5c6ae6e1fe4494d1053356f4cc958cde9
amartyapandey7681 <amartyapandey@gmail.com>
2021-01-12 11:23:40 +0530

    first commit

---
+++ middleware/authJwt.js
@@ -0,0 +1,92 @@
+const promise=require("bluebird");
+
+const jwt = promise.promisify(require("jsonwebtoken"));
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided!"
+    });
+  }
+
+ 
+  }).then( jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!"
+      });
+    }
+    req.userId = decoded.id;
+    next()).catch(function(err){
+      console.log("error")
+    })
+
+isAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Admin Role!"
+      });
+      return;
+    });
+  });
+};
+
+isModerator = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "moderator") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Moderator Role!"
+      });
+    });
+  });
+};
+
+isModeratorOrAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "moderator") {
+          next();
+          return;
+        }
+
+        if (roles[i].name === "admin") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Moderator or Admin Role!"
+      });
+    });
+  });
+};
+
+const authJwt = {
+  verifyToken: verifyToken,
+  isAdmin: isAdmin,
+  isModerator: isModerator,
+  isModeratorOrAdmin: isModeratorOrAdmin
+};
+module.exports = authJwt;
